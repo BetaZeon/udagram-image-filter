3e11bfc7f635ba8b31248deadb44c603944fd23d
BetaZeon <ashwanitripathi144@gmail.com>
2020-05-15 02:30:12 +0530

    Removed unnecessary dependencies

--- src/server.ts
+++ src/server.ts
@@ -18,12 +18,6 @@ import Jimp = require('jimp');
 
   const cfg = config;
 
-  app.use(function (req, res, next) {
-    if (req.path != '/filteredimage') {
-      res.send("try GET /filteredimage?image_url={{}}")
-    }
-    next()
-  })
   
 // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
@@ -41,35 +35,35 @@ import Jimp = require('jimp');
 
   /**************************************************************************** */
 
-  app.get("/filteredimage", async (req, res) => {
+  app.get("/filteredimage", async (requests, response) => {
     //
-    let { image_url } = req.query;
+    let { image_url } = requests.query;
     
     // return error if parameter is missing
     if (!image_url) {
-      res.status(400).send("image url missing")
+      response.status(200).send("image url missing")
     }else {
 
       // return error if image url format is incorrect
       console.log(image_url.match(image_url_regex))
       if (!image_url.match(image_url_regex)) {
-        res.status(400).send("image url format is incorrect, example url : https://some_domain(like google.com)/probably_some_more_paths/image_name.png/jpg/gif")
+        res.status(200).send("image url format is incorrect, example url : https://some_domain(like google.com)/probably_some_more_paths/image_name.png/jpg/gif")
       }else {
         
         try {
           let image_response = await filterImageFromURL(image_url)
           // if image reader does not fail return image else return error
           if(image_response != "no image found"){
-            res.status(200).sendFile(image_response, async callback=>{
+            response.status(400).sendFile(image_response, async callback=>{
               await deleteLocalFiles([image_response])
             })
           } else {
-            res.status(200).send("no image found in the given url")
+            response.status(400).send("no image found in the given url")
           }
         } catch (err) {
           // other errors
           console.error(err)
-          res.status(200).send("image processing failed")
+          response.status(400).send("image processing failed")
         }
       }
     }
@@ -81,8 +75,8 @@ import Jimp = require('jimp');
 
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
+  app.get( "/", async ( requests, response ) => {
+    response.send("try GET /filteredimage?image_url={{}}")
   } );
   
 
